abd8d2a3057cd280bec731a69ec03bb14bc2441f
jiamin-he <mandymandyiloveyou@163.com>
2020-06-14 17:05:32 -0700

    set backfill_size to 6

--- crawler_36kr.py
+++ crawler_36kr.py
@@ -13,6 +13,7 @@ import pytz
 
 tag_start_with = "window.initialState="
 calendar_id = '95nhljg3dp6ui5cojv2728n50o@group.calendar.google.com'
+backfill_size = 6
 
 def calender_initiate():
     creds = None
@@ -28,7 +29,7 @@ def calender_initiate():
 
 # backfill
 def backfill(url,service):
-    payload = {"partner_id":"web","timestamp":1592097024336,"param":{"searchType":"article","searchWord":"氪星晚报","sort":"date","pageSize":4,"pageEvent":1,"pageCallback":"eyJmaXJzdElkIjo3MDg3ODM3ODMzNjkyMjIsImxhc3RJZCI6NjY3NjI0NjI3MDk5NjUwLCJmaXJzdENyZWF0ZVRpbWUiOjE1ODk1MzgwODQzNTMsImxhc3RDcmVhdGVUaW1lIjoxNTg3MTE5NTIyNjM2fQ","siteId":1,"platformId":2}}
+    payload = {"partner_id":"web","timestamp":1592097024336,"param":{"searchType":"article","searchWord":"氪星晚报","sort":"date","pageSize":backfill_size,"pageEvent":1,"pageCallback":"eyJmaXJzdElkIjo3MDg3ODM3ODMzNjkyMjIsImxhc3RJZCI6NjY3NjI0NjI3MDk5NjUwLCJmaXJzdENyZWF0ZVRpbWUiOjE1ODk1MzgwODQzNTMsImxhc3RDcmVhdGVUaW1lIjoxNTg3MTE5NTIyNjM2fQ","siteId":1,"platformId":2}}
     headers = {"content-type":'application/json','User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
     r = requests.post(url,json = payload, headers = headers)
 
